02e65c2c2ae6eb2dcdb29a3e5038073eebeff716
sava <savamedvedev@gmail.com>
2022-08-21 20:43:28 +0800

    second-commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,14 @@ import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 // import Newsletter from "../components/Newsletter";
 // import Products from "../components/Products";
-// import Slider from "../components/Slider";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
       <Footer />
     </div>
   );
