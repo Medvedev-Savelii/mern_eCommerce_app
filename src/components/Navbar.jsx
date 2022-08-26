6904bbb00409c6f9d80e650c1fc987fd30e56f8d
sava <savamedvedev@gmail.com>
2022-08-26 20:51:21 +0800

    13-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { mobile } from "../responsive";
@@ -68,6 +69,8 @@ const MenuItem = styled.div`
 `;
 
 const Navbar = () => {
+  const cart = useSelector((state) => state.cart);
+
   return (
     <Container>
       <Wrapper>
@@ -100,7 +103,7 @@ const Navbar = () => {
           </Link>
           <Link to='/cart'>
             <MenuItem>
-              <Badge badgeContent={4} color='primary'>
+              <Badge badgeContent={cart.quantity} color='primary'>
                 <ShoppingCartOutlined />
               </Badge>
             </MenuItem>
